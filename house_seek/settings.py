377beced0f8b186438782f3c67286efa9651dd12
xinya0liu <susanna0liu@gmail.com>
2024-12-18 14:19:02 +1000

    added local  settings import

--- house_seek/settings.py
+++ house_seek/settings.py
@@ -153,3 +153,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
